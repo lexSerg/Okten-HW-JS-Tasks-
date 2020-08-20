5239fba5de6492d0e1f049ce26bffa7024a27376
lexSerg <serginko21@gmail.com>
2020-08-20 18:13:17 +0300

    add task 1 at vue(min) repository

--- draft.js
+++ draft.js
@@ -1,5 +1,13 @@
-let date = new Date;
-//let formatDate = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
-let strDate = date.toLocaleDateString();
-let strTime = date.toLocaleTimeString()
-console.log(`${strDate}, ${strTime}`)
+// let date = new Date;
+// //let formatDate = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+// let strDate = date.toLocaleDateString();
+// let strTime = date.toLocaleTimeString()
+// console.log(`${strDate}, ${strTime}`)
+let regExp = /[\w\.\,]/g;
+let str = 'Hello, Mr. Smith'
+let res = str.match(regExp);
+console.log(res);
+
+let s = '';
+s = String.fromCharCode(1103,1104,1105);
+console.log(s);
